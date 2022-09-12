85e4497cb1d0e6d0142459fbd202eb83a11f2bcf
Sebastian MacDonald <optissimum@gmail.com>
2022-09-12 21:35:38 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-86GUwKPnbzM-ybFAvF-7kI25gAGdNj9vSYt_jor4Nc/edit#gid=745832354';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyAGA_K_F14nccIljq02zzmSXa9IMPgbzeA';
