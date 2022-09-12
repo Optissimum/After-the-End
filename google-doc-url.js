eeec3906cc114e4e258ee7b33da106a1d9df1b3c
Sebastian MacDonald <optissimum@gmail.com>
2022-09-12 21:50:58 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-86GUwKPnbzM-ybFAvF-7kI25gAGdNj9vSYt_jor4Nc/edit#gid=745832354';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-86GUwKPnbzM-ybFAvF-7kI25gAGdNj9vSYt_jor4Nc/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAGA_K_F14nccIljq02zzmSXa9IMPgbzeA';
